a20e751b737fbc322839045708b3792785b0547b
sachin supunthaka <sachinsupunthaka96@gmail.com>
2023-01-09 12:08:19 +0530

    update: chakra provider wrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { type AppType } from "next/dist/shared/lib/utils";
 
 import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default MyApp;
